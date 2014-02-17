f1ec704bcec8080f4d604be0a129cd274c1cbf64
Marco Miglierina <marco.miglierina@icloud.com>
2014-02-17 01:59:00 +0100

    License Fix. Lib and templates unzipped.

--- src/main/java/it/polimi/modaclouds/cpimlibrary/blobmng/GlassfishBlobManagerFactory.java
+++ src/main/java/it/polimi/modaclouds/cpimlibrary/blobmng/GlassfishBlobManagerFactory.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2013 deib-polimi
+ * Contact: deib-polimi <marco.miglierina@polimi.it>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package it.polimi.modaclouds.cpimlibrary.blobmng;
 
 import java.sql.Connection;
@@ -9,7 +25,7 @@ public class GlassfishBlobManagerFactory extends CloudBlobManagerFactory {
 
 	String blobConnectionString=null;
 	
-	//questo metodo crea nel database identificato dalla stringa di connessione passata una tabella che andrà a contenere i blob
+	//questo metodo crea nel database identificato dalla stringa di connessione passata una tabella che andrï¿½ a contenere i blob
 	//istanzia quindi un nuovo glassfishblob manager passando la tringa di connessione in quanto necessaria per l inserimento dei blob
 	public GlassfishBlobManagerFactory(String blobConnectionString) {
 		
