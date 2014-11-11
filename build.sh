60422205a2c2b2b46a28adc61b1bd53d5b4f0543
Fabio Arcidiacono <fa.arcidiacono@gmail.com>
2014-11-11 16:48:39 +0100

    updated pom, JPA 2.1 resolves complilation errors

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-mvn package -Dmaven.javadoc.skip=true && echo '' && cp target/cpim-library-1.0-SNAPSHOT.jar  ../kundera-test/lib/  && echo '.jar copied to lib/' && cp target/cpim-library-1.0-SNAPSHOT.jar  ../kundera-test/war/WEB-INF/lib/ && echo '.jar copied to war/WEB-INF/lib/'
+#!/bin/sh
+mvn package -Dmaven.javadoc.skip=true && echo '' && cp target/cpim-library-2.0-SNAPSHOT.jar  ../kundera-test/lib/  && echo '.jar copied to lib/' && cp target/cpim-library-2.0-SNAPSHOT.jar  ../kundera-test/war/WEB-INF/lib/ && echo '.jar copied to war/WEB-INF/lib/'
