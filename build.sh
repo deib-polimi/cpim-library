438c764aca10ca79fa0b4c13de759db2f312f6c3
Fabio Arcidiacono <fa.arcidiacono@gmail.com>
2014-12-15 17:36:49 +0100

    added license header

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn package -DskipTests -Dmaven.javadoc.skip=true && echo '' && cp target/cpim-library-2.0-SNAPSHOT.jar  ../kundera-test/lib/  && echo '.jar copied to lib/' && cp target/cpim-library-2.0-SNAPSHOT.jar  ../kundera-test/war/WEB-INF/lib/ && echo '.jar copied to war/WEB-INF/lib/'
+mvn package -DskipTests && echo '' && cp target/cpim-library-2.0-SNAPSHOT.jar  ../kundera-test/lib/  && echo '.jar copied to lib/' && cp target/cpim-library-2.0-SNAPSHOT.jar  ../kundera-test/war/WEB-INF/lib/ && echo '.jar copied to war/WEB-INF/lib/'
