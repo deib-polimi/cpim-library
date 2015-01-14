1b0cefb65c2703892dd3bd14961466fb53f25299
Fabio Arcidiacono <fa.arcidiacono@gmail.com>
2015-01-14 13:41:42 +0100

    more test

---
+++ src/test/java/it/polimi/modaclouds/cpimlibrary/entitymng/tests/QueryStringTest.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2013 deib-polimi
+ * Contact: deib-polimi <marco.miglierina@polimi.it>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package it.polimi.modaclouds.cpimlibrary.entitymng.tests;
+
+import it.polimi.modaclouds.cpimlibrary.entitymng.statements.DeleteStatement;
+import it.polimi.modaclouds.cpimlibrary.entitymng.statements.InsertStatement;
+import it.polimi.modaclouds.cpimlibrary.entitymng.statements.UpdateStatement;
+import it.polimi.modaclouds.cpimlibrary.entitymng.statements.utils.CompareOperator;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Fabio Arcidiacono.
+ */
+public class QueryStringTest {
+
+    @Test
+    public void testInsert() {
+        InsertStatement insert = new InsertStatement();
+        insert.setTable("Test");
+        insert.addField("name", "Fabio");
+        insert.addField("salary", 123L);
+
+        Assert.assertEquals("INSERT INTO Test (name, salary) VALUES ('Fabio', '123')", insert.toString());
+    }
+
+    @Test
+    public void testUpdate() {
+        UpdateStatement update = new UpdateStatement();
+        update.setTable("Test");
+        update.addField("name", "Fabio");
+        update.addField("salary", 123L);
+        update.addCondition("salary", CompareOperator.GREATER_THAN_OR_EQUAL, 45L);
+
+        Assert.assertEquals("UPDATE Test SET name = 'Fabio', salary = '123' WHERE salary >= '45'", update.toString());
+    }
+
+    @Test
+    public void testDelete() {
+        DeleteStatement delete = new DeleteStatement();
+        delete.setTable("Test");
+        delete.addCondition("salary", CompareOperator.LOWER_THAN_OR_EQUAL, 45L);
+
+        Assert.assertEquals("DELETE FROM Test WHERE salary <= '45'", delete.toString());
+    }
+}
